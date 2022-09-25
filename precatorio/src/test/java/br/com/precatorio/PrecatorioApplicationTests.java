2ddc392574f0b4b2d7f2db1242f1e211917b2e17
Thiago Perrut <thi.perrut@gmail.com>
2022-09-25 16:12:07 -0300

    exceptions comuns ao sistema

---
+++ precatorio/src/test/java/br/com/precatorio/PrecatorioApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.precatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecatorioApplicationTests {
+
+//	@Test 	void contextLoads() {	}
+
+}
