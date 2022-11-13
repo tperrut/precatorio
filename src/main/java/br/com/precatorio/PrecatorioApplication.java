d7b56f7536538e611cd50d7ecc1546e188ba4878
Thiago Perrut <thi.perrut@gmail.com>
2022-11-13 19:53:48 -0300

    add config do Cors no Aplication

--- src/main/java/br/com/precatorio/PrecatorioApplication.java
+++ src/main/java/br/com/precatorio/PrecatorioApplication.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PrecatorioApplication {
@@ -12,6 +15,16 @@ public class PrecatorioApplication {
     public static void main(String[] args) {
         log.info("Vamo q vamo com Thiagão do GONGOLO!!!!!!!");
         SpringApplication.run(PrecatorioApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/*").allowedOrigins("https://precatorio-app.herokuapp.com");
+            }
+        };
     }
 
 }
