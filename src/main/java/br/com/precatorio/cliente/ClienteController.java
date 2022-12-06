7069d991f2649396d5cba1c9d62c343e79b6ebe6
Thiago Perrut <thi.perrut@gmail.com>
2022-12-06 20:53:02 -0300

    cors de prod

--- src/main/java/br/com/precatorio/cliente/ClienteController.java
+++ src/main/java/br/com/precatorio/cliente/ClienteController.java
@@ -13,8 +13,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@CrossOrigin(origins = "https://precatorio-app.herokuapp.com")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://precatorio-app.herokuapp.com")
+//@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 @RequestMapping("/api/v1/")
